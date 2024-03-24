b01653a0869de1c7a755a23f901f793980267dd9
Cory House <housecor@gmail.com>
2024-03-24 12:24:45 -0500

    gen data

---
+++ ts-app-demo/ts-app-demo/src/Faker.tsx
@@ -0,0 +1,34 @@
+import { faker } from "@faker-js/faker";
+import { Product } from "./Detail";
+
+export default function Faker() {
+  function generateFakeData() {
+    const products: Product[] = [];
+
+    for (let i = 1; i < 100; i++) {
+      products.push({
+        id: i,
+        name: faker.commerce.productName(),
+        description: faker.commerce.productDescription(),
+        image: faker.image.urlLoremFlickr(),
+        price: parseInt(faker.commerce.price()),
+        category: faker.commerce.department(),
+        skus: [
+          {
+            sku: faker.number.int().toString(),
+            size: faker.number.int({ min: 1, max: 15 }),
+          },
+        ],
+      });
+    }
+    return products;
+  }
+
+  const fakeData = generateFakeData();
+  return (
+    <>
+      <h1>Fake products data</h1>
+      {JSON.stringify(fakeData)}
+    </>
+  );
+}
