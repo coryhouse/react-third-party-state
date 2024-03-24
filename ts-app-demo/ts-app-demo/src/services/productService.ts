9338105ec831378c714c3c91dec76f8431ecb431
Cory House <housecor@gmail.com>
2024-03-24 16:47:45 -0500

    a

--- ts-app-demo/ts-app-demo/src/services/productService.ts
+++ ts-app-demo/ts-app-demo/src/services/productService.ts
@@ -1,12 +1,12 @@
-const baseUrl = process.env.REACT_APP_API_BASE_URL;
+const baseUrl = import.meta.env.REACT_APP_API_BASE_URL;
 
-export async function getProducts(category) {
+export async function getProducts(category: string) {
   const response = await fetch(baseUrl + "products?category=" + category);
   if (response.ok) return response.json();
   throw response;
 }
 
-export async function getProduct(id) {
+export async function getProduct(id: number) {
   const response = await fetch(baseUrl + "products/" + id);
   if (response.ok) return response.json();
   throw response;
