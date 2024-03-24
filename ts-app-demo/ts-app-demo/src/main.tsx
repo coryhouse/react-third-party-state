c7727958877a8166cf0c9ff992cac91e361b0e9c
Cory House <housecor@gmail.com>
2024-03-24 16:53:28 -0500

    a

--- ts-app-demo/ts-app-demo/src/main.tsx
+++ ts-app-demo/ts-app-demo/src/main.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import ErrorBoundary from "./ErrorBoundary";
 import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "./cartContext";
 import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <BrowserRouter>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </BrowserRouter>
-    </ErrorBoundary>
+    <BrowserRouter>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
