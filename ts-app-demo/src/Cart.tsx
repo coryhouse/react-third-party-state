010f4e53a0cba737d0d4b89b7f3733d7025ed90c
Cory House <housecor@gmail.com>
2024-05-05 15:02:24 -0500

    build fix

--- ts-app-demo/src/Cart.tsx
+++ ts-app-demo/src/Cart.tsx
@@ -1,14 +1,38 @@
 import Spinner from "./Spinner";
 import { useNavigate } from "react-router-dom";
 import { useCart } from "./cartContext";
-import useFetch from "./services/useFetch";
 import { CartItem, Product } from "./types/types";
+import { useEffect, useState } from "react";
 
 export default function Cart() {
   const { cart, setCart } = useCart();
   const navigate = useNavigate();
-  const url = "products?" + cart.map(({ id }) => "id=" + id).join("&");
-  const { data: products, loading, error } = useFetch<Product[]>(url);
+
+  const [products, setProducts] = useState<Product[] | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const url =
+          import.meta.env.VITE_APP_API_BASE_URL +
+          "products?" +
+          cart.map(({ id }) => "id=" + id).join("&");
+        const data = await fetch(url);
+        if (!data.ok) {
+          throw new Error(`Product not found: ${data.status}`);
+        }
+        const products = await data.json();
+        setProducts(products);
+      } catch (error) {
+        setError(error as Error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
 
   function renderItem(itemInCart: CartItem, product: Product) {
     const { sku, quantity } = itemInCart;
