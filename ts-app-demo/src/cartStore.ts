f39da3405b2fdd18b4a689500d9eadbc1e52d2c2
Cory House <housecor@gmail.com>
2024-05-05 13:36:29 -0500

    add

--- ts-app-demo/src/cartStore.ts
+++ ts-app-demo/src/cartStore.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { CartItem } from "./types/types";
+import { persist } from "zustand/middleware";
 
 type Store = {
   cart: CartItem[];
@@ -8,27 +9,33 @@ type Store = {
   addToCart: (id: number, sku: string) => void;
 };
 
-export const useCartStore = create<Store>((set) => ({
-  cart: [],
-  emptyCart: () => set({ cart: [] }),
-  updateCartQuantity: (sku: string, quantity: number) => {
-    set(({ cart }) => ({
-      cart:
-        quantity === 0
-          ? cart.filter((i) => i.sku !== sku)
-          : cart.map((i) => (i.sku === sku ? { ...i, quantity } : i)),
-    }));
-  },
-  addToCart: (id: number, sku: string) => {
-    set((state) => {
-      const itemInCart = state.cart.find((i) => i.sku === sku);
-      return {
-        cart: itemInCart
-          ? state.cart.map((i) =>
-              i.sku === sku ? { ...i, quantity: i.quantity + 1 } : i
-            )
-          : [...state.cart, { id, sku, quantity: 1 }],
-      };
-    });
-  },
-}));
+// Show without persist first, then add persist and 2nd argument to store in localStorage.
+export const useCartStore = create(
+  persist<Store>(
+    (set) => ({
+      cart: [],
+      emptyCart: () => set({ cart: [] }),
+      updateCartQuantity: (sku: string, quantity: number) => {
+        set(({ cart }) => ({
+          cart:
+            quantity === 0
+              ? cart.filter((i) => i.sku !== sku)
+              : cart.map((i) => (i.sku === sku ? { ...i, quantity } : i)),
+        }));
+      },
+      addToCart: (id: number, sku: string) => {
+        set((state) => {
+          const itemInCart = state.cart.find((i) => i.sku === sku);
+          return {
+            cart: itemInCart
+              ? state.cart.map((i) =>
+                  i.sku === sku ? { ...i, quantity: i.quantity + 1 } : i
+                )
+              : [...state.cart, { id, sku, quantity: 1 }],
+          };
+        });
+      },
+    }),
+    { name: "cart" }
+  )
+);
