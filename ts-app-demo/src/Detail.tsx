6fb7be848ed78de69bdb6c4cce24a8774556e6c4
Cory House <housecor@gmail.com>
2024-05-18 13:59:38 -0500

    refactor

--- ts-app-demo/src/Detail.tsx
+++ ts-app-demo/src/Detail.tsx
@@ -12,52 +12,44 @@ export default function Detail() {
 
   const { data: product } = useGetProductById(id);
 
+  if (!product || !id) return <PageNotFound />;
+
   return (
-    <>
-      {!product || !id ? (
-        <PageNotFound />
-      ) : (
-        <div id="detail">
-          <h1>{product.name}</h1>
-          <p>{product.description}</p>
-          <p id="price">${product.price}</p>
+    <div id="detail">
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p id="price">${product.price}</p>
 
-          <select
-            id="size"
-            value={sku}
-            onChange={(e) => setSku(e.target.value)}
-          >
-            <option value="">What size?</option>
-            {product.skus.map((s) => (
-              <option key={s.sku} value={s.sku}>
-                {s.size}
-              </option>
-            ))}
-          </select>
+      <select id="size" value={sku} onChange={(e) => setSku(e.target.value)}>
+        <option value="">What size?</option>
+        {product.skus.map((s) => (
+          <option key={s.sku} value={s.sku}>
+            {s.size}
+          </option>
+        ))}
+      </select>
 
-          <p>
-            <button
-              disabled={!sku}
-              className="btn btn-primary"
-              onClick={() => {
-                if (!sku) return alert("Select size.");
-                setCart((cart) => {
-                  const itemInCart = cart.find((i) => i.sku === sku);
-                  return itemInCart
-                    ? cart.map((i) =>
-                        i.sku === sku ? { ...i, quantity: i.quantity + 1 } : i
-                      )
-                    : [...cart, { id: parseInt(id), sku, quantity: 1 }];
-                });
-                navigate("/cart");
-              }}
-            >
-              Add to cart
-            </button>
-          </p>
-          <img src={`/images/${product.image}`} alt={product.category} />
-        </div>
-      )}
-    </>
+      <p>
+        <button
+          disabled={!sku}
+          className="btn btn-primary"
+          onClick={() => {
+            if (!sku) return alert("Select size.");
+            setCart((cart) => {
+              const itemInCart = cart.find((i) => i.sku === sku);
+              return itemInCart
+                ? cart.map((i) =>
+                    i.sku === sku ? { ...i, quantity: i.quantity + 1 } : i
+                  )
+                : [...cart, { id: parseInt(id), sku, quantity: 1 }];
+            });
+            navigate("/cart");
+          }}
+        >
+          Add to cart
+        </button>
+      </p>
+      <img src={`/images/${product.image}`} alt={product.category} />
+    </div>
   );
 }
