6cdf41a10c28b497958fd6c4d56e03c58d18a2f3
Cory House <housecor@gmail.com>
2024-05-18 16:16:04 -0500

    fix

--- ts-app-demo/src/cartState.ts
+++ ts-app-demo/src/cartState.ts
@@ -16,19 +16,26 @@ subscribe(cartState, () => {
   localStorage.setItem("cart", JSON.stringify(cartState));
 });
 
+function getItemIndexBySku(sku: string) {
+  return cartState.cart.findIndex((i) => i.sku === sku);
+}
+
 // Utility functions below here. Note that all these calls are mutative which we can safely do because Valtio uses a proxy.
 export function addToCart(id: number, sku: string) {
-  cartState.cart.push({ id, sku, quantity: 1 });
+  const itemIndex = getItemIndexBySku(sku);
+  if (itemIndex > -1) {
+    cartState.cart[itemIndex]!.quantity += 1;
+  } else {
+    cartState.cart.push({ id, sku, quantity: 1 });
+  }
 }
 
 export function updateQuantity(sku: string, quantity: number) {
-  const itemIndex = cartState.cart.findIndex((i) => i.sku === sku);
-  const item = cartState.cart[itemIndex];
-  if (!item) throw new Error("Sku not found in cart");
+  const itemIndex = getItemIndexBySku(sku);
   if (quantity === 0) {
     cartState.cart.splice(itemIndex, 1); // Remove item from cart by index
   } else {
-    item.quantity = quantity;
+    cartState.cart[itemIndex]!.quantity = quantity;
   }
 }
 
