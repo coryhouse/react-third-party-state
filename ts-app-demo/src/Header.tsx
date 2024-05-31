e37a61f2549a4ff0116ec2fb779369360e2ec9c0
Cory House <housecor@gmail.com>
2024-05-31 14:59:02 -0500

    through clip 2

--- ts-app-demo/src/Header.tsx
+++ ts-app-demo/src/Header.tsx
@@ -1,10 +1,11 @@
 import { Link, NavLink } from "react-router-dom";
 import { useCart } from "./context/cartContext";
-import { useUser } from "./context/userContext";
+import { useAtom } from "jotai";
+import { userAtom } from "./atoms/userAtom";
 
 export default function Header() {
   const { cart } = useCart();
-  const { user, setUser } = useUser();
+  const [user, setUser] = useAtom(userAtom);
 
   return (
     <header>
