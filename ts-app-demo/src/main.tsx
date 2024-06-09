f0c02a0708c5a45c7b86f5172d54ce13447a2528
Cory House <housecor@gmail.com>
2024-06-09 07:49:48 -0500

    add

--- ts-app-demo/src/main.tsx
+++ ts-app-demo/src/main.tsx
@@ -7,6 +7,7 @@ import { ErrorBoundary } from "react-error-boundary";
 import { Toaster } from "react-hot-toast";
 import { UserProvider } from "./context/userContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -24,6 +25,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           <UserProvider>
             <Toaster />
             <Routes />
+            <ReactQueryDevtools />
           </UserProvider>
         </CartProvider>
       </QueryClientProvider>
